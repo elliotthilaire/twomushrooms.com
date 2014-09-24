c1f6c1b333221faa171c1f78b3b8e5284a09a877
elliotthilaire <elliott.hilaire@gmail.com>
2014-09-24 23:49:28 +1000

    tidied things up

--- app.rb
+++ app.rb
@@ -60,30 +60,8 @@ get "/about/" do
   redirect '/about'
 end
 
-post '/contact' do 
-
-    Pony.mail(
-      :from => "<smtp.elliott.hilaire@gmail.com>",
-      :to => 'elliott.hilaire@gmail.com',
-      :subject => "[Photography] #{params[:email]}",
-      :body => params[:message],
-#      :port => '587',
-#      :via => :smtp,
-#      :via_options => { 
-#        :address              => 'smtp.gmail.com', 
-#        :port                 => '587', 
-#        :enable_starttls_auto => true, 
-#        :user_name            => 'smtp.elliott.hilaire@gmail.com', 
-#        :password             => 'fingerbuns', 
-#        :authentication       => :plain, 
-#        :domain               => 'localhost.localdomain'
-#      }
-)
-end
-
-
-get "/:photo" do
-  @photo = Photo.find(params[:photo]) || raise(Sinatra::NotFound)
+get "/:slug" do
+  @photo = Photo.find(params[:slug]) || raise(Sinatra::NotFound)
   erb :photo
 end
 
@@ -95,16 +73,6 @@ error do
   erb :error
   #env['sinatra.error'].name
 end
-
-#get "/blog" do
-#  @posts = Post.all
-#  erb :posts
-#end
-
-#get "/blog/:slug" do
-#  @post = Post.find(params[:slug]) || raise(Sinatra::NotFound)
-#  erb :post
-#end
 
 #get "/content/*" do
 # file = "content/" + params[:splat].first
