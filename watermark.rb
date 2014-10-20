b0f9f6420095c88248ecc4ed3e731ba7c25b637c
elliotthilaire <elliott.hilaire@gmail.com>
2014-10-20 17:07:47 +1000

    made dragonfly work with watermarks

--- watermark.rb
+++ watermark.rb
@@ -1,35 +1,35 @@
 class WatermarkProcessor
 
+## not needed for sinatra
 ##include Configurable
 ##include ImageMagick::Utils
 
   def call(content, *args)
-    
-	content.process! :convert, "'public/watermark.png' -gravity southeast -composite"
 
-	# command = "#{Sinatra::Thumbnails.settings.convert_executable} \"#{src}\" \"./#{Sinatra::Thumbnails.settings.watermark}\" -gravity southeast -composite -define jpeg:size=1200x1200  -thumbnail #{format} \"#{dest}\""
+    # uses the source image dimensions to resize the watermark with
+	#image_properties = identify(content)
+	watermark_resize = "100x100" #{}"#{image_properties[900]}x#{image_properties[10]}^"
+	
 
 
+
+	content.process! :convert, "'public/watermark.png' -gravity southeast  -compose dissolve -define compose:args='100' -composite"
   end
 
   def update_url(attrs, *args) # attrs is Job#url_attributes, which is an OpenStruct-like object
     attrs.style = 'watermarked'
   end
 
-	def watermark(source_image, opts = {})
+  def watermark(source_image, opts = {})
 	opts = defaults.merge(opts)
 	
-	# uses the source image dimensions to resize the watermark with
-	image_properties = identify(source_image)
-	watermark_resize = "#{image_properties[:width]}x#{image_properties[:height]}^"
 	
 	# assembles an ImageMagick convert command which does all the work
 	convert(
 	source_image,
 	"( #{opts[:watermark]} -resize #{watermark_resize} ) -compose dissolve -define compose:args=#{opts[:opacity]} -composite"
 	)
-	# as you can see the convert syntax is somewhat inscrutable, which is why
-	# it's called ImageMagick and not ImageObvious
+
 	end
 
 	def defaults
