2c6d5e29f5e9d9aceebe2b1417030ed8747883e8
elliotthilaire <elliott.hilaire@gmail.com>
2014-10-31 07:51:22 +1000

    updated watermark_process.rb

--- dragonfly/watermark_processor.rb
+++ dragonfly/watermark_processor.rb
@@ -3,7 +3,8 @@ class WatermarkProcessor
     content.process! :convert, "'public/watermark.png' -gravity southeast -composite"
   end
 
-  def update_url(attrs, *_args) # attrs is Job#url_attributes, which is an OpenStruct-like object
+ # attrs is Job#url_attributes, which is an OpenStruct-like object
+  def update_url(attrs, *_args)
     attrs.style = 'watermarked'
   end
 end
