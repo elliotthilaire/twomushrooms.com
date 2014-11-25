8642a76d77c74ec996dc6cfec20a74d328941adb
elliotthilaire <elliott.hilaire@gmail.com>
2014-11-25 18:18:23 +1000

    tidied dragonfly initializer

--- dragonfly/initializer.rb
+++ dragonfly/initializer.rb
@@ -4,28 +4,19 @@ require './dragonfly/orientation_analyser'
 
 Dragonfly.app.configure do
 
-
-
   response_header 'Cache-Control', 'public, max-age=86400'
 
   secret ENV['DRAGONFLY_SECRET']
-  #verify_urls false
 
   plugin :imagemagick
 
-  #url_format '/media/:job/:name'
   url_format "/media/:job/:sha/:name"
 
-
-
   fetch_file_whitelist [
-    /.*/
+    /content/
   ]
 
-  datastore :file, root_path: './content/'
-
   processor :watermark, WatermarkProcessor.new
-
   analyser :orientation, OrientationAnalyser.new
 
 end
