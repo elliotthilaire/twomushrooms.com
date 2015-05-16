467ab8a7a4f2c98c1825ac30d870250d3f053e20
elliotthilaire <elliott.hilaire@gmail.com>
2015-05-16 17:11:55 +1000

    refactor photo class

--- app/photo.rb
+++ app/photo.rb
@@ -2,7 +2,7 @@ require 'exifr'
 
 class Photo
 
-  attr_accessor :pathname, :category, :slug, :filename, :title, :caption, :date_taken
+  attr_reader :pathname, :category, :slug, :filename, :title, :caption, :date_taken
 
   @path = './content' # no trailing slash
 
@@ -43,12 +43,8 @@ class Photo
   # return an array of photos from multiple categories
   # categories taken in as an array of strings
   # e.g. find_by_categories(['featured', 'gallery'])
-  def self.find_by_categories(array)
-    photos = []
-    array.each do |category| 
-      photos.push *find_by_category(category)
-    end
-    photos
+  def self.find_by_categories(categories)
+    categories.reduce([]) { |photos, category| photos.push *find_by_category(category) }  
   end
 
   # return a single photo object
@@ -58,7 +54,7 @@ class Photo
     prepare(slug: slug).first || nil
   end
 
-  protected
+  private
 
   # return an array of photo objects from the file system
   # takes a params hash to filter category or slug
@@ -68,9 +64,10 @@ class Photo
     # glob search string from parameters
     category = params[:category] || '*'
     slug = params[:slug] || '*'
+
     search_string = "#{@path}/#{category}/#{slug}.{jpg}"  
-    
     Dir.glob(search_string).collect { |pathname| new(pathname: pathname) }
   end
+
 
 end
