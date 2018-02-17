978b903c0e982c8218aa8472ee163c027930d364
Elliott Hilaire <elliott.hilaire@gmail.com>
2018-02-17 19:28:24 +0000

    Crawl error page

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 wget --mirror --page-requisites --directory=./_build/ localhost:9292
+
+wget --content-on-error --directory-prefix=./_build/ localhost:9292/error.html
+mv ./_build/error.html ./_build/localhost:9292/error.html
